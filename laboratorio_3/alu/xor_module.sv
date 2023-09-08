177b0868ecacaa8df1662c2c6e84a5a6427a1d4e
Kevinfgn <31755700+Kevinfgn@users.noreply.github.com>
2023-09-08 16:14:11 -0600

    Finalización modulo XOR

--- laboratorio_3/alu/xor_module.sv
+++ laboratorio_3/alu/xor_module.sv
@@ -1,4 +1,4 @@
-module xor_module #(parameter N = 4) (
+/*module xor_module #(parameter N = 4) (
   input wire [N-1:0] a,
   input wire [N-1:0] b,
   output wire [N-1:0] y
@@ -6,4 +6,22 @@ module xor_module #(parameter N = 4) (
 
   assign y = a ^ b;
 
+endmodule*/
+
+
+module xor_module #(parameter N = 4) (
+  input wire [N-1:0] a,
+  input wire [N-1:0] b,
+  output wire [N-1:0] y
+);
+
+  genvar i;
+  generate
+    for (i = 0; i < N; i = i + 1) begin : bit_and_generate
+      assign y[i] = a[i] ^ b[i];
+    end
+  endgenerate
+
 endmodule
+
+
