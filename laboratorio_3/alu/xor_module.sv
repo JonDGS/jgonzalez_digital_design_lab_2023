2fb02d969186b5dfbd9fe3347bc97b9f45178f29
Kevinfgn <31755700+Kevinfgn@users.noreply.github.com>
2023-09-08 16:15:16 -0600

    ignore

--- laboratorio_3/alu/xor_module.sv
+++ laboratorio_3/alu/xor_module.sv
@@ -1,14 +1,3 @@
-/*module xor_module #(parameter N = 4) (
-  input wire [N-1:0] a,
-  input wire [N-1:0] b,
-  output wire [N-1:0] y
-);
-
-  assign y = a ^ b;
-
-endmodule*/
-
-
 module xor_module #(parameter N = 4) (
   input wire [N-1:0] a,
   input wire [N-1:0] b,
