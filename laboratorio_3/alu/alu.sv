acf82f58013243c7ee1f6b4f891ab41dfe0b8deb
sergio <sergiorc2000@hotmail.es>
2023-09-09 20:14:27 -0600

    Union de shifts a ALU, eliminacion de carpeta de shifts

--- laboratorio_3/alu/alu.sv
+++ laboratorio_3/alu/alu.sv
@@ -1,12 +1,14 @@
 module alu #(parameter N = 4) (
   input wire [N-1:0] a,
   input wire [N-1:0] b,
+  input wire [N-1:0] data_shifts,
+  input wire [N-1:0] shift_number,
   input wire [3:0] operation,  // Código de control para la operación (0: AND, 1: OR, 2: XOR, Faltan agregar los demás)
   output reg [N-1:0] y,
   output logic [6:0] display1
 );
 
-  wire [N-1:0] and_result, or_result, xor_result;
+  wire [N-1:0] and_result, or_result, xor_result, shiftLeft_result,shiftRight_result;
   logic [3:0] displayResult1;
 
   and_module #(N) and_inst (
@@ -27,12 +29,29 @@ module alu #(parameter N = 4) (
     .y(xor_result)
   );
   
+  Shifts #(.n(N)) shiftLeft_inst (
+		.data(data_shifts),
+		.shift(shift_number),
+		.direccion(0),
+		.y(shiftLeft_result)
+   );
+	
+	Shifts #(.n(N)) shiftRight_inst (
+		.data(data_shifts),
+		.shift(shift_number),
+		.direccion(1),
+		.y(shiftRight_result)
+   );
+  
 
   always @* begin
     case (operation)
       4'b0000: y = and_result; // AND
       4'b0001: y = or_result;  // OR
       4'b0010: y = xor_result; // XOR
+		4'b0010: y = xor_result; // XOR
+		4'b0011: y = shiftLeft_result; // Shift Left
+		4'b0100: y = shiftRight_result; // Shift Right
       default: y = 0;          // Otras operaciones personalizables
     endcase
   end
--- laboratorio_3/alu/alu_tb.sv
+++ laboratorio_3/alu/alu_tb.sv
@@ -6,6 +6,8 @@ module alu_tb;
   // Señales de entrada
   reg [N-1:0] a;
   reg [N-1:0] b;
+  reg [N-1:0] data_shifts;
+  reg [N-1:0] shift_number;
   reg [3:0] operation; // Código de control para la operación (0: AND, 1: OR, 2: XOR)
 
   // Señal de salida
@@ -15,6 +17,8 @@ module alu_tb;
   alu #(N) dut (
     .a(a),
     .b(b),
+	 .data_shifts(data_shifts),
+	 .shift_number(shift_number),
     .operation(operation),
     .y(y)
   );
@@ -51,6 +55,27 @@ module alu_tb;
     else
       $display("Prueba de XOR correcta. a=%b, b=%b, y=%b", a, b, y);
 
+		
+	 // Prueba de SHIFT LEFT
+    data_shifts=4'b1110;
+    operation = 4'b0011; // SHIFT LEFT
+	 shift_number=2;
+    #10; // Espera 10 unidades de tiempo
+	 if (y !== 4'b1000)
+      $display("Prueba de SHIFT LEFT fallida. Data=%b,Resultado=%b", data_shifts, y);
+    else
+      $display("Prueba de SHIFT LEFT correcta. Data=%b,Resultado=%b", data_shifts, y);
+		
+	 // Prueba de SHIFT RIGHT
+    data_shifts=4'b0100;
+    operation = 4'b0100; // SHIFT RIGHT
+	 shift_number=2;
+    #10; // Espera 10 unidades de tiempo
+	 if (y !== 4'b0001)
+      $display("Prueba de SHIFT RIGHT fallida. Data=%b,Resultado=%b", data_shifts, y);
+    else
+      $display("Prueba de SHIFT RIGHT correcta. Data=%b,Resultado=%b", data_shifts, y);
+	 
     // Agrega más pruebas según sea necesario
 
   end
