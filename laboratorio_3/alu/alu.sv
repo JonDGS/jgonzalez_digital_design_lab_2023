2bd627e841862081fd18570371572452255b55b7
Kevinfgn <31755700+Kevinfgn@users.noreply.github.com>
2023-09-12 17:02:14 -0600

    Asignacion de pines

--- laboratorio_3/alu/alu.sv
+++ laboratorio_3/alu/alu.sv
@@ -1,13 +1,12 @@
 module alu #(parameter N = 4) (
   input wire [N-1:0] a,
   input wire [N-1:0] b,
-  input wire [N-1:0] data_shifts,
-  input wire [N-1:0] shift_number,
   input wire [3:0] operation,  // Código de control para la operación (0: AND, 1: OR, 2: XOR, Faltan agregar los demás)
   output reg [N-1:0] y,
-  output logic carry,
+  output logic carryOut,
   output logic zero,
-  output logic overflow,
+  output logic overflowAdd,
+  output logic overflowSub, 
   output logic negative,
   output logic [6:0] display1
 );
@@ -34,15 +33,15 @@ module alu #(parameter N = 4) (
   );
   
   Shifts #(.n(N)) shiftLeft_inst (
-		.data(data_shifts),
-		.shift(shift_number),
+		.data(a),
+		.shift(b),
 		.direccion(0),
 		.y(shiftLeft_result)
    );
 	
 	Shifts #(.n(N)) shiftRight_inst (
-		.data(data_shifts),
-		.shift(shift_number),
+		.data(a),
+		.shift(b),
 		.direccion(1),
 		.y(shiftRight_result)
    );
@@ -59,19 +58,23 @@ module alu #(parameter N = 4) (
   .B(b),
   .K(1'b0),
   .S(adder_result),
-  .Cout(overflow)
+  .Cout(carryOut),
+  .Overflow(overflowAdd)
   );
   
-  fourBitFullAdder #(N) restador_inst(
+    fourBitFullAdder #(N) sub_inst(
   .A(a),
   .B(b),
   .K(1'b1),
   .S(sub_result),
-  .Cout(negative)
+  .Cout(negative),
+  .Overflow(overflowSub)
   );
   
-  
+  assign overflow = overflowAdd | overflowSub;
+    
   always @* begin
+  
     case (operation)
       4'b0000: y = and_result; // AND
       4'b0001: y = or_result;  // OR
@@ -82,10 +85,16 @@ module alu #(parameter N = 4) (
 		4'b0101: y = mod_result; // Residuo
 		4'b0110: y = adder_result; // Suma
 		4'b0111: y = sub_result; // Resta
-
-      default: y = 0;          // Otras operaciones personalizables
+      4'b1111: y = 0;
     endcase
+	 
+	 if (y == 4'b0000) begin
+        zero = 1'b1; // Establecer la bandera en 1 si el resultado es cero
+    end else begin
+        zero = 1'b0; // Establecer la bandera en 0 si el resultado no es cero
+    end	 
   end
+  
   
   
   always_comb begin
--- laboratorio_3/alu/fourBitFullAdder.sv
+++ laboratorio_3/alu/fourBitFullAdder.sv
@@ -4,7 +4,8 @@ module fourBitFullAdder #(parameter n = 4)
 							input logic [n-1:0] B,
 							input logic K,
 							output logic [n:0] S,
-							output logic Cout
+							output logic Cout,
+							output logic Overflow
 							);
 							
 							wire C [n-1:0];
@@ -14,13 +15,14 @@ module fourBitFullAdder #(parameter n = 4)
 							generate
 								for (i = 0; i < n; i++) begin : fa
 									if (i == 0) begin
-										fullAdder fa(.A(A[i]), .B(B[i]),.Cin(K), .S(S[i]), .Cout(C[i]));
+										fullAdder fa(.A(A[i]), .B(B[i] ^ K),.Cin(K), .S(S[i]), .Cout(C[i]));
 									end else begin
-										fullAdder fa(.A(A[i]), .B(B[i]),.Cin(C[i-1]), .S(S[i]), .Cout(C[i]));
+										fullAdder fa(.A(A[i]), .B(B[i] ^ K),.Cin(C[i-1]), .S(S[i]), .Cout(C[i]));
 									end
 								end
 							endgenerate
 							
 							assign Cout = C[n-1];
+							assign Overflow = C[n-1] ^ C[n-2];
 							
 endmodule
