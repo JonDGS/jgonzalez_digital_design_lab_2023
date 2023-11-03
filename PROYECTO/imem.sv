cb549e7d5c5eaae9825bf3250821fa63e8b6b3ce
sergio <sergiorc2000@hotmail.es>
2023-11-02 19:08:45 -0600

    Proc v0.2
    Co-Authored-By: Kevinfgn <31755700+Kevinfgn@users.noreply.github.com>

--- PROYECTO/imem.sv
+++ PROYECTO/imem.sv
@@ -2,6 +2,10 @@ module imem(input logic [31:0] a,
 output logic [31:0] rd);
 logic [31:0] RAM[63:0];
 initial
-$readmemh("memfile.dat",RAM);
+
+//CAMBIAR DIRECCION DEL ARCHIVO ========================
+$readmemh("C:/Users/sergi/Desktop/TEC/2023/2/TALLER/LABS/GIT/jgonzalez_digital_design_lab_2023/PROYECTO/memfile.dat",RAM);
+//CAMBIAR DIRECCION DEL ARCHIVO ========================
+
 assign rd = RAM[a[31:2]]; // word aligned
 endmodule
